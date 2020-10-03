948de48c1ba59ba55d093583eeb4707d7c226881
xiluisx <xiluisx1234@gmail.com>
2020-10-03 13:23:35 -0500

    Added search command and moved files to better paths

---
+++ ClassLibrary8/Commands/Search.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Text;
+using BrokeProtocol.API;
+using BrokeProtocol.Entities;
+
+namespace ClassLibrary8.Commands
+{
+    public class Search : IScript
+    {
+        public Search()
+        {
+            CommandHandler.RegisterCommand("search", new Action<ShPlayer, string>(OnSearchMessage));
+        }
+
+        private void OnSearchMessage(ShPlayer player, string message)
+        {
+            var results = Core.Instance.MessagesQueue.Messages.Where(x=>x.Content.Contains(message));
+            var sb = new StringBuilder();
+            foreach(var result in results)
+            {
+                sb.AppendLine($"{result.Author}: {result.Content}");
+            }
+            player.svPlayer.SendTextPanel($"Searching: {message}", sb.ToString());
+        }
+    }
+}
---
+++ ClassLibrary8/Message.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary8
+{
+    public class MessagesQueue
+    {
+        private Queue<Message> messages = new Queue<Message>();
+
+        public void AddMessage(Message message)
+        {
+            if (messages.Count >= 300)
+            {
+                messages.Dequeue();
+            }
+
+            messages.Enqueue(message);
+        }
+
+        public Message[] Messages
+        {
+            get { return messages.Reverse().ToArray(); }
+        }
+    }
+
+    public class Message
+    {
+        public string Author { get; set; }
+
+        public string Content { get; set; }
+
+        public Message(string Author, string Content)
+        {
+            this.Author = Author;
+            this.Content = Content;
+        }
+    }
+}
